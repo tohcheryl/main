f02f9878b49380bdee6a81ff94b216f24034ee7e
Sam Xie <samxie@outlook.com.au>
2018-03-13 23:34:53 +0800

    Food card tests - replaced person variables to food context

--- src/test/java/seedu/address/ui/FoodCardTest.java
+++ src/test/java/seedu/address/ui/FoodCardTest.java
@@ -17,56 +17,56 @@ public class FoodCardTest extends GuiUnitTest {
     public void display() {
         // no tags
         Food foodWithNoTags = new FoodBuilder().withTags(new String[0]).build();
-        FoodCard personCard = new FoodCard(foodWithNoTags, 1);
-        uiPartRule.setUiPart(personCard);
-        assertCardDisplay(personCard, foodWithNoTags, 1);
+        FoodCard foodCard = new FoodCard(foodWithNoTags, 1);
+        uiPartRule.setUiPart(foodCard);
+        assertCardDisplay(foodCard, foodWithNoTags, 1);
 
         // with tags
         Food foodWithTags = new FoodBuilder().build();
-        personCard = new FoodCard(foodWithTags, 2);
-        uiPartRule.setUiPart(personCard);
-        assertCardDisplay(personCard, foodWithTags, 2);
+        foodCard = new FoodCard(foodWithTags, 2);
+        uiPartRule.setUiPart(foodCard);
+        assertCardDisplay(foodCard, foodWithTags, 2);
     }
 
     @Test
     public void equals() {
         Food food = new FoodBuilder().build();
-        FoodCard personCard = new FoodCard(food, 0);
+        FoodCard foodCard = new FoodCard(food, 0);
 
         // same food, same index -> returns true
         FoodCard copy = new FoodCard(food, 0);
-        assertTrue(personCard.equals(copy));
+        assertTrue(foodCard.equals(copy));
 
         // same object -> returns true
-        assertTrue(personCard.equals(personCard));
+        assertTrue(foodCard.equals(foodCard));
 
         // null -> returns false
-        assertFalse(personCard.equals(null));
+        assertFalse(foodCard.equals(null));
 
         // different types -> returns false
-        assertFalse(personCard.equals(0));
+        assertFalse(foodCard.equals(0));
 
         // different food, same index -> returns false
         Food differentFood = new FoodBuilder().withName("differentName").build();
-        assertFalse(personCard.equals(new FoodCard(differentFood, 0)));
+        assertFalse(foodCard.equals(new FoodCard(differentFood, 0)));
 
         // same food, different index -> returns false
-        assertFalse(personCard.equals(new FoodCard(food, 1)));
+        assertFalse(foodCard.equals(new FoodCard(food, 1)));
     }
 
     /**
-     * Asserts that {@code personCard} displays the details of {@code expectedFood} correctly and matches
+     * Asserts that {@code foodCard} displays the details of {@code expectedFood} correctly and matches
      * {@code expectedId}.
      */
-    private void assertCardDisplay(FoodCard personCard, Food expectedFood, int expectedId) {
+    private void assertCardDisplay(FoodCard foodCard, Food expectedFood, int expectedId) {
         guiRobot.pauseForHuman();
 
-        FoodCardHandle personCardHandle = new FoodCardHandle(personCard.getRoot());
+        FoodCardHandle foodCardHandle = new FoodCardHandle(foodCard.getRoot());
 
         // verify id is displayed correctly
-        assertEquals(Integer.toString(expectedId) + ". ", personCardHandle.getId());
+        assertEquals(Integer.toString(expectedId) + ". ", foodCardHandle.getId());
 
         // verify food details are displayed correctly
-        assertCardDisplaysFood(expectedFood, personCardHandle);
+        assertCardDisplaysFood(expectedFood, foodCardHandle);
     }
 }
