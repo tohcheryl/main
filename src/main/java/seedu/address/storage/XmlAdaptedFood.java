15951f50d308dbde9432ae204331e30a0d84dac5
Jackson Huang <jackson_huang@live.com>
2018-03-13 18:22:11 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/storage/XmlAdaptedFood.java
+++ src/main/java/seedu/address/storage/XmlAdaptedFood.java
@@ -11,8 +11,8 @@ import javax.xml.bind.annotation.XmlElement;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Food;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
