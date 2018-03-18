d25f4348ed9ff5f21274e342841d44cb8a05e754
Sam Xie <samxie@outlook.com.au>
2018-03-18 16:46:26 +0800

    Added Food Property and basic FoodPropertyTest cases

---
+++ src/main/java/seedu/address/model/food/FoodProperty.java
@@ -0,0 +1,53 @@
+package seedu.address.model.food;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Food's property in HackEat
+ */
+public abstract class FoodProperty {
+
+    public static final String MESSAGE_CONSTRAINT =
+            "To be implemented by child classes";
+    public static final String PROPERTY_VALID_REGEX = "[^\\s].*";
+    public final String value;
+
+    /**
+     * Constructs a {@code FoodProperty}.
+     *
+     * @param foodProperty A valid FoodProperty.
+     */
+    public FoodProperty(String foodProperty) {
+        requireNonNull(foodProperty);
+        checkArgument(isValid(foodProperty, PROPERTY_VALID_REGEX), MESSAGE_CONSTRAINT);
+        this.value = foodProperty;
+    }
+
+
+    /**
+     * Returns true if a given string is a valid food property.
+     */
+    public static boolean isValid (String test, String regex) {
+        return test.matches(regex);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Phone // instanceof handles nulls
+                && this.value.equals(((Phone) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+}
---
+++ src/test/java/seedu/address/model/food/FoodPropertyTest.java
@@ -0,0 +1,62 @@
+package seedu.address.model.food;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+public class FoodPropertyTest {
+
+    private static final String FOOD_STUB_MESSAGE_CONSTRAINT = "Message constraint example.";
+    private static final String FOOD_STUB_VALID_REGEX = "[^\\s].*";
+
+    @Test
+    public void constructor_test() {
+        FoodPropertyStub foodPropertyStub = new FoodPropertyStub("Test");
+
+        // Test value
+        assertEquals(foodPropertyStub.value, "Test");
+
+        // Test MESSAGE_STUB_CONSTRAINTS override
+        assertEquals(foodPropertyStub.MESSAGE_CONSTRAINT, FOOD_STUB_MESSAGE_CONSTRAINT);
+
+        // Test PROPERTY_VALID_REGEX override
+        assertEquals(foodPropertyStub.PROPERTY_VALID_REGEX, FOOD_STUB_VALID_REGEX);
+    }
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new FoodPropertyStub(null));
+    }
+
+    @Test
+    public void constructor_invalidFoodPropertyStub_throwsIllegalArgumentException() {
+        String invalidFoodStubValue = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new FoodPropertyStub(invalidFoodStubValue));
+    }
+
+    @Test
+    public void isValid() {
+        // TODO: Add isValid for all child classes
+
+        // invalid address
+        assertFalse(FoodProperty.isValid("", Address.ADDRESS_VALIDATION_REGEX)); // empty string
+
+        // valid addresses
+        assertTrue(FoodProperty.isValid("Blk 456, Den Road, #01-355", Address.ADDRESS_VALIDATION_REGEX));
+    }
+
+    private class FoodPropertyStub extends FoodProperty {
+
+        public static final String MESSAGE_CONSTRAINT = FOOD_STUB_MESSAGE_CONSTRAINT;
+        public static final String PROPERTY_VALID_REGEX = FOOD_STUB_VALID_REGEX;
+
+        public FoodPropertyStub(String property) {
+            super(property);
+        }
+
+    }
+}
