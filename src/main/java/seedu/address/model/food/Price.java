ebc1338423886b60f47bcdc931a87367ed89ec68
tohcheryl <e0035693@u.nus.edu>
2018-03-19 20:39:46 +0800

    Add Price class Add apache commons library validator to gradle

---
+++ src/main/java/seedu/address/model/food/Price.java
@@ -0,0 +1,113 @@
+package seedu.address.model.food;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
+
+import org.apache.commons.validator.routines.BigDecimalValidator;
+import org.apache.commons.validator.routines.CurrencyValidator;
+
+/**
+ * Represents a Food's price in HackEat.
+ * Guarantees: immutable; is valid as declared in {@link #isValidPrice(String)}
+ */
+public class Price {
+
+    public static final String MESSAGE_PRICE_CONSTRAINTS =
+            "Price can only contain currency symbol and numbers";
+    private static Locale currentLocale = Locale.getDefault();
+    private BigDecimal value;
+
+    /**
+     * Constructs a {@code Price}.
+     *
+     * @param price A valid price.
+     */
+    public Price(String price) {
+        requireNonNull(price);
+        checkArgument(isValidPrice(price), MESSAGE_PRICE_CONSTRAINTS);
+        setPrice(price);
+    }
+
+    /**
+     * Returns true if a given string begins/ends with the correct currency symbol.
+     */
+    private static boolean isValidSymbol(String inputPrice) {
+        Currency currency = Currency.getInstance(currentLocale);
+        String symbol = currency.getSymbol();
+        if (inputPrice.startsWith(symbol) || inputPrice.endsWith(symbol)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns true if a given string is a valid price.
+     */
+    public static boolean isValidPrice(String inputPrice) {
+        BigDecimal amount = null;
+        BigDecimalValidator validator = CurrencyValidator.getInstance();
+        if (isValidSymbol(inputPrice)) {
+            amount = validator.validate(inputPrice, currentLocale);
+        } else {
+            amount = null;
+        }
+
+        if (amount != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Sets price of Food without currency symbol.
+     * Truncates given price if it is given to more than 2 decimal places(if applicable).
+     */
+    public void setPrice(String inputPrice) {
+        BigDecimalValidator validator = CurrencyValidator.getInstance();
+        value = validator.validate(inputPrice, currentLocale);
+    }
+
+    /**
+     * Sets locale
+     */
+    public static void setCurrentLocale(Locale locale) {
+        currentLocale = locale;
+    }
+
+    /**
+     * Returns current locale
+     */
+    public static Locale getCurrentLocale() {
+        return currentLocale;
+    }
+
+    /**
+     * Returns price of Food as a BigDecimal.
+     */
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Price // instanceof handles nulls
+                && this.value.equals(((Price) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
---
+++ src/test/java/seedu/address/model/food/PriceTest.java
@@ -0,0 +1,78 @@
+package seedu.address.model.food;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+public class PriceTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Price(null));
+    }
+
+    @Test
+    public void constructor_invalidPrice_throwsIllegalArgumentException() {
+        String invalidPrice = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Price(invalidPrice));
+    }
+
+    @Test
+    public void isValidPrice() {
+        // null price
+        Assert.assertThrows(NullPointerException.class, () -> Price.isValidPrice(null));
+
+        // invalid prices in Singapore
+        assertFalse(Price.isValidPrice("12a.45"));
+        assertFalse(Price.isValidPrice("$12a.45"));
+        assertFalse(Price.isValidPrice("$1.p0"));
+        assertFalse(Price.isValidPrice("203$0"));
+        assertFalse(Price.isValidPrice("10$"));
+        assertFalse(Price.isValidPrice("12.40$"));
+
+        // valid prices in Singapore
+        assertTrue(Price.isValidPrice("$20"));
+        assertTrue(Price.isValidPrice("$90.30"));
+        assertTrue(Price.isValidPrice("$20.590"));
+    }
+
+    @Test
+    public void setPrice() {
+        Price p = new Price("$23.40");
+        BigDecimal expectedP = new BigDecimal("23.40");
+        assertEquals(expectedP, p.getValue());
+    }
+
+    @Test
+    public void getValue() {
+        Price p = new Price("$23.40");
+        BigDecimal expectedP = new BigDecimal("23.40");
+        assertEquals(expectedP, p.getValue());
+    }
+
+    @Test
+    public void toString_validPrice_returnsString() {
+        Price p = new Price("$23.40");
+        assertEquals("23.40", p.toString());
+    }
+
+    @Test
+    public void equals_validPrice_returnsEqual() {
+        Price p = new Price("$23.40");
+        Price p2 = new Price("$23.40");
+        assertEquals(p, p2);
+    }
+
+    @Test
+    public void hashCode_validPrice_returnsHashCode() {
+        Price p = new Price("$23.40");
+        Price p2 = new Price("$23.40");
+        assertEquals(p.hashCode(), p2.hashCode());
+    }
+}
