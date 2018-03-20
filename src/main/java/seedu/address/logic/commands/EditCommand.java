4ff2c3946f2ef3983c79cbe55c49418c885fceb7
Sam Xie <samxie@outlook.com.au>
2018-03-20 15:54:24 +0800

    Fixed edit command to read prefix r/ for rating

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -172,7 +172,8 @@ public class EditCommand extends UndoableCommand {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(this.name, this.phone, this.email, this.address, this.price, this.tags);
+            return CollectionUtil.isAnyNonNull(this.name, this.phone, this.email, this.address, this.price, this.rating,
+                    this.tags);
         }
 
         public void setName(Name name) {
