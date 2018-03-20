a4707721c863c8ee0262974c0bf05b0a1f11de7e
Sam Xie <samxie@outlook.com.au>
2018-03-20 15:18:37 +0800

    Fixed parser to parse rating correctly

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,6 +6,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRICE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RATING;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
@@ -36,7 +37,7 @@ public class AddCommandParser implements Parser<AddCommand> {
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                        PREFIX_ADDRESS, PREFIX_PRICE, PREFIX_TAG);
+                        PREFIX_ADDRESS, PREFIX_PRICE, PREFIX_RATING, PREFIX_TAG);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_PHONE)
                 || !argMultimap.getPreamble().isEmpty()) {
@@ -53,7 +54,7 @@ public class AddCommandParser implements Parser<AddCommand> {
                     .orElse(new Address("UNKNOWN"));
             Price price = ParserUtil.parsePrice(argMultimap.getValue(PREFIX_PRICE))
                     .orElse(new Price("$0"));
-            Rating rating = ParserUtil.parseRating(argMultimap.getValue(PREFIX_PRICE))
+            Rating rating = ParserUtil.parseRating(argMultimap.getValue(PREFIX_RATING))
                     .orElse(new Rating("0"));
 
             Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
