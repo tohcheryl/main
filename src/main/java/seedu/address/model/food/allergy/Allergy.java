bf7b03fe823906096cbf940015884cbfa37315d4
Jackson Huang <jackson_huang@live.com>
2018-03-20 21:46:50 +0800

    Implemented Allergy class

---
+++ src/main/java/seedu/address/model/food/allergy/Allergy.java
@@ -0,0 +1,55 @@
+package seedu.address.model.food.allergy;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents an Allergy in HackEat.
+ * Guarantees: immutable; name is valid as declared in {@link #isValidAllergyName(String)}
+ */
+public class Allergy {
+
+    public static final String MESSAGE_TAG_CONSTRAINTS = "Allergys names should be alphanumeric";
+    public static final String TAG_VALIDATION_REGEX = "\\p{Alnum}+";
+
+    public final String allergyName;
+
+    /**
+     * Constructs a {@code Allergy}.
+     *
+     * @param allergyName A valid allergy name.
+     */
+    public Allergy(String allergyName) {
+        requireNonNull(allergyName);
+        checkArgument(isValidAllergyName(allergyName), MESSAGE_TAG_CONSTRAINTS);
+        this.allergyName = allergyName;
+    }
+
+    /**
+     * Returns true if a given string is a valid allergy name.
+     */
+    public static boolean isValidAllergyName(String test) {
+        return test.matches(TAG_VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Allergy // instanceof handles nulls
+                && this.allergyName.equals(((Allergy) other).allergyName)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return allergyName.hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    public String toString() {
+        return '[' + allergyName + ']';
+    }
+
+}
+
