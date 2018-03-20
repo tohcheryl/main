0147aa1764d9ad2a005d5f8d33b7ccdbe3b2283a
Sam Xie <samxie@outlook.com.au>
2018-03-20 15:22:51 +0800

    Add Command Parser Test has cases for Ratings now. Also fixed test case for price where it has invalid address instead of invalid price

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -10,6 +10,7 @@ import static seedu.address.logic.commands.CommandTestUtil.INVALID_EMAIL_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_PHONE_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_PRICE_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_RATING_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_TAG_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_BANANA;
@@ -19,6 +20,8 @@ import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.commands.CommandTestUtil.PRICE_DESC_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.PRICE_DESC_BANANA;
+import static seedu.address.logic.commands.CommandTestUtil.RATING_DESC_APPLE;
+import static seedu.address.logic.commands.CommandTestUtil.RATING_DESC_BANANA;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIED;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_NUTS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_APPLE;
@@ -31,6 +34,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BANANA;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PRICE_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PRICE_BANANA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_RATING_APPLE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_RATING_BANANA;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIED;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_NUTS;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -39,11 +44,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.Test;
 
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.model.food.Address;
-import seedu.address.model.food.Email;
-import seedu.address.model.food.Food;
-import seedu.address.model.food.Name;
-import seedu.address.model.food.Phone;
+import seedu.address.model.food.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.FoodBuilder;
 
@@ -54,34 +55,40 @@ public class AddCommandParserTest {
     public void parse_allFieldsPresent_success() {
         Food expectedFood = new FoodBuilder().withName(VALID_NAME_BANANA).withPhone(VALID_PHONE_BANANA)
                 .withEmail(VALID_EMAIL_BANANA).withAddress(VALID_ADDRESS_BANANA)
-                .withPrice(VALID_PRICE_BANANA).withTags(VALID_TAG_FRIED).build();
+                .withPrice(VALID_PRICE_BANANA).withRating(VALID_RATING_BANANA).withTags(VALID_TAG_FRIED).build();
 
         // whitespace only preamble
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + TAG_DESC_FRIED, new AddCommand(expectedFood));
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BANANA + PHONE_DESC_BANANA
+                        + EMAIL_DESC_BANANA + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA
+                        + TAG_DESC_FRIED, new AddCommand(expectedFood));
 
         // multiple names - last name accepted
         assertParseSuccess(parser, NAME_DESC_APPLE + NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + TAG_DESC_FRIED, new AddCommand(expectedFood));
+                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_FRIED,
+                new AddCommand(expectedFood));
 
         // multiple phones - last phone accepted
         assertParseSuccess(parser, NAME_DESC_BANANA + PHONE_DESC_APPLE + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + TAG_DESC_FRIED, new AddCommand(expectedFood));
+                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_FRIED,
+                new AddCommand(expectedFood));
 
         // multiple emails - last email accepted
         assertParseSuccess(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_APPLE + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + TAG_DESC_FRIED, new AddCommand(expectedFood));
+                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_FRIED,
+                new AddCommand(expectedFood));
 
         // multiple addresses - last address accepted
-        assertParseSuccess(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA + ADDRESS_DESC_APPLE
-                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + TAG_DESC_FRIED, new AddCommand(expectedFood));
+        assertParseSuccess(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                        + ADDRESS_DESC_APPLE + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA
+                        + TAG_DESC_FRIED, new AddCommand(expectedFood));
 
         // multiple tags - all accepted
         Food expectedFoodMultipleTags = new FoodBuilder().withName(VALID_NAME_BANANA).withPhone(VALID_PHONE_BANANA)
                 .withEmail(VALID_EMAIL_BANANA).withAddress(VALID_ADDRESS_BANANA).withPrice(VALID_PRICE_BANANA)
-                .withTags(VALID_TAG_FRIED, VALID_TAG_NUTS).build();
-        assertParseSuccess(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA + ADDRESS_DESC_BANANA
-                + PRICE_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED, new AddCommand(expectedFoodMultipleTags));
+                .withRating(VALID_RATING_BANANA).withTags(VALID_TAG_FRIED, VALID_TAG_NUTS).build();
+        assertParseSuccess(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED,
+                new AddCommand(expectedFoodMultipleTags));
     }
 
     @Test
@@ -89,9 +96,9 @@ public class AddCommandParserTest {
         // zero tags
         Food expectedFood = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE)
                 .withEmail(VALID_EMAIL_APPLE).withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE)
-                .withTags().build();
+                .withRating(VALID_RATING_APPLE).withTags().build();
         assertParseSuccess(parser, NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE
-                        + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE,
+                        + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE,
                 new AddCommand(expectedFood));
 
         // missing email prefix
@@ -105,6 +112,14 @@ public class AddCommandParserTest {
                 .withEmail(VALID_EMAIL_APPLE).withPrice(VALID_PRICE_APPLE).withTags().build();
         assertParseSuccess(parser, NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + PRICE_DESC_APPLE,
                 new AddCommand(expectedFoodNoAddress));
+
+
+        // missing rating
+        Food expectedFoodNoRating = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE)
+                .withAddress(VALID_ADDRESS_APPLE).withEmail(VALID_EMAIL_APPLE).withPrice(VALID_PRICE_APPLE)
+                .withTags().build();
+        assertParseSuccess(parser, NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
+                        + PRICE_DESC_APPLE, new AddCommand(expectedFoodNoRating));
     }
 
     @Test
@@ -127,36 +142,48 @@ public class AddCommandParserTest {
     @Test
     public void parse_invalidValue_failure() {
         // invalid name
-        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BANANA + EMAIL_DESC_BANANA + ADDRESS_DESC_BANANA
-                + PRICE_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED, Name.MESSAGE_NAME_CONSTRAINTS);
+        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                        + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_NUTS
+                        + TAG_DESC_FRIED, Name.MESSAGE_NAME_CONSTRAINTS);
 
         // invalid phone
-        assertParseFailure(parser, NAME_DESC_BANANA + INVALID_PHONE_DESC + EMAIL_DESC_BANANA + ADDRESS_DESC_BANANA
-                + PRICE_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED, Phone.MESSAGE_PHONE_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_BANANA + INVALID_PHONE_DESC + EMAIL_DESC_BANANA
+                        + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_NUTS
+                + TAG_DESC_FRIED, Phone.MESSAGE_PHONE_CONSTRAINTS);
 
         // invalid email
-        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + INVALID_EMAIL_DESC + ADDRESS_DESC_BANANA
-                + PRICE_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED, Email.MESSAGE_EMAIL_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + INVALID_EMAIL_DESC
+                        + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_NUTS
+                + TAG_DESC_FRIED, Email.MESSAGE_EMAIL_CONSTRAINTS);
 
         // invalid address
-        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA + INVALID_ADDRESS_DESC
-                + PRICE_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED, Address.MESSAGE_ADDRESS_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                        + INVALID_ADDRESS_DESC + PRICE_DESC_BANANA + RATING_DESC_BANANA + TAG_DESC_NUTS
+                + TAG_DESC_FRIED, Address.MESSAGE_ADDRESS_CONSTRAINTS);
 
         // invalid price
-        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA + INVALID_ADDRESS_DESC
-                + INVALID_PRICE_DESC + TAG_DESC_NUTS + TAG_DESC_FRIED, Address.MESSAGE_ADDRESS_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                        + ADDRESS_DESC_BANANA + INVALID_PRICE_DESC + RATING_DESC_BANANA + TAG_DESC_NUTS
+                + TAG_DESC_FRIED, Price.MESSAGE_PRICE_CONSTRAINTS);
+
+        // invalid rating
+        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                        + ADDRESS_DESC_BANANA + VALID_PRICE_BANANA + INVALID_RATING_DESC + TAG_DESC_NUTS
+                        + TAG_DESC_FRIED, Rating.MESSAGE_RATING_CONSTRAINTS);
 
         // invalid tag
-        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA + ADDRESS_DESC_BANANA
-                + PRICE_DESC_BANANA + INVALID_TAG_DESC + VALID_TAG_FRIED, Tag.MESSAGE_TAG_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
+                        + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + INVALID_TAG_DESC
+                        + VALID_TAG_FRIED, Tag.MESSAGE_TAG_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
-                        + INVALID_ADDRESS_DESC + PRICE_DESC_BANANA, Name.MESSAGE_NAME_CONSTRAINTS);
+                        + INVALID_ADDRESS_DESC + PRICE_DESC_BANANA + RATING_DESC_BANANA, Name.MESSAGE_NAME_CONSTRAINTS);
 
         // non-empty preamble
-        assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BANANA + PHONE_DESC_BANANA + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + TAG_DESC_NUTS + TAG_DESC_FRIED,
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BANANA + PHONE_DESC_BANANA
+                        + EMAIL_DESC_BANANA + ADDRESS_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA
+                        + TAG_DESC_NUTS + TAG_DESC_FRIED,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
     }
 }
