794f0141df4368edd9cfcfd6d899d37c0b3da3cc
Sam Xie <samxie@outlook.com.au>
2018-03-20 12:41:07 +0800

    Sample data inclues rating

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -11,6 +11,7 @@ import seedu.address.model.food.Food;
 import seedu.address.model.food.Name;
 import seedu.address.model.food.Phone;
 import seedu.address.model.food.Price;
+import seedu.address.model.food.Rating;
 import seedu.address.model.food.exceptions.DuplicateFoodException;
 import seedu.address.model.tag.Tag;
 
@@ -21,22 +22,22 @@ public class SampleDataUtil {
     public static Food[] getSampleFoods() {
         return new Food[] {
             new Food(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), new Price("$0"),
+                new Address("Blk 30 Geylang Street 29, #06-40"), new Price("$0"), new Rating("0"),
                     getTagSet("friends")),
             new Food(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Price("$0"),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Price("$0"), new Rating("0"),
                     getTagSet("colleagues", "friends")),
             new Food(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Price("$0"),
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Price("$0"), new Rating("0"),
                     getTagSet("neighbours")),
             new Food(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Price("$0"),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Price("$0"), new Rating("0"),
                     getTagSet("family")),
             new Food(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), new Price("$0"),
+                new Address("Blk 47 Tampines Street 20, #17-35"), new Price("$0"), new Rating("0"),
                     getTagSet("classmates")),
             new Food(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), new Price("$0"),
+                new Address("Blk 45 Aljunied Street 85, #11-31"), new Price("$0"), new Rating("0"),
                     getTagSet("colleagues"))
         };
     }
