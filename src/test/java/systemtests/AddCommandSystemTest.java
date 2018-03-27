31a89e072645a5d0e09cdfa0e87cdcd8f82f6c65
tohcheryl <e0035693@u.nus.edu>
2018-03-27 21:25:17 +0800

    Fix AddCommandSystemTest by updating FoodUtil to include Allergy

--- src/test/java/systemtests/AddCommandSystemTest.java
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -3,6 +3,7 @@ package systemtests;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_BANANA;
+import static seedu.address.logic.commands.CommandTestUtil.ALLERGY_DESC_LACTOSE;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_BANANA;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_ADDRESS_DESC;
@@ -24,6 +25,7 @@ import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIED;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_NUTS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BANANA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ALLERGY_LACTOSE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_APPLE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BANANA;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_APPLE;
@@ -78,7 +80,7 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         Food toAdd = APPLE;
         String command = "   " + AddCommand.COMMAND_WORD + "  " + NAME_DESC_APPLE + "  " + PHONE_DESC_APPLE + " "
                 + EMAIL_DESC_APPLE + "   " + ADDRESS_DESC_APPLE + "   " + PRICE_DESC_APPLE + "   "
-                + RATING_DESC_APPLE + "  " + TAG_DESC_FRIED + " ";
+                + RATING_DESC_APPLE + "  " + TAG_DESC_FRIED + " " + ALLERGY_DESC_LACTOSE + " ";
         assertCommandSuccess(command, toAdd);
 
         /* Case: undo adding Amy to the list -> Amy deleted */
@@ -95,49 +97,49 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         /* Case: add a food with all fields same as another food in HackEat except name -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_BANANA).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_APPLE)
                 .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_BANANA + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food with all fields same as another food in HackEat except phone -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_BANANA).withEmail(VALID_EMAIL_APPLE)
                 .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_BANANA + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food with all fields same as another food in HackEat except email -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_BANANA)
                 .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_BANANA + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food with all fields same as another food in HackEat except address -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_APPLE)
                 .withAddress(VALID_ADDRESS_BANANA).withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_BANANA
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food with all fields same as another food in HackEat except price -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_APPLE)
                 .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_BANANA).withRating(VALID_RATING_APPLE)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_BANANA + RATING_DESC_APPLE + TAG_DESC_FRIED;
+                + PRICE_DESC_BANANA + RATING_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food with all fields same as another food in HackEat except rating -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_APPLE)
                 .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_BANANA)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_BANANA + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_BANANA + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add to empty address book -> added */
@@ -147,7 +149,7 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         /* Case: add a food with tags, command with parameters in random order -> added */
         toAdd = BANANA;
         command = AddCommand.COMMAND_WORD + TAG_DESC_FRIED + PHONE_DESC_BANANA + ADDRESS_DESC_BANANA + NAME_DESC_BANANA
-                + TAG_DESC_NUTS + EMAIL_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA;
+                + TAG_DESC_NUTS + EMAIL_DESC_BANANA + PRICE_DESC_BANANA + RATING_DESC_BANANA + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* -------------------------- Perform add with missing fields (optional fields) ------------------------------*/
@@ -158,30 +160,33 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         /* Case: add a food, missing email -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE)
                 .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE)
-                .withTags(VALID_TAG_FRIED).build();
+                .withTags(VALID_TAG_FRIED).withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food, missing address -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_BANANA)
-                .withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE).withTags(VALID_TAG_FRIED).build();
+                .withPrice(VALID_PRICE_APPLE).withRating(VALID_RATING_APPLE).withTags(VALID_TAG_FRIED)
+                .withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_BANANA
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE  + TAG_DESC_FRIED;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE  + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food, missing price -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_BANANA)
-                .withAddress(VALID_ADDRESS_APPLE).withRating(VALID_RATING_APPLE).withTags(VALID_TAG_FRIED).build();
+                .withAddress(VALID_ADDRESS_APPLE).withRating(VALID_RATING_APPLE).withTags(VALID_TAG_FRIED)
+                .withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_APPLE + RATING_DESC_APPLE  + TAG_DESC_FRIED;
+                + ADDRESS_DESC_APPLE + RATING_DESC_APPLE  + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* Case: add a food, missing rating -> added */
         toAdd = new FoodBuilder().withName(VALID_NAME_APPLE).withPhone(VALID_PHONE_APPLE).withEmail(VALID_EMAIL_BANANA)
-                .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withTags(VALID_TAG_FRIED).build();
+                .withAddress(VALID_ADDRESS_APPLE).withPrice(VALID_PRICE_APPLE).withTags(VALID_TAG_FRIED)
+                .withAllergies(VALID_ALLERGY_LACTOSE).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_BANANA
-                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + TAG_DESC_FRIED;
+                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + TAG_DESC_FRIED + ALLERGY_DESC_LACTOSE;
         assertCommandSuccess(command, toAdd);
 
         /* -------------------------- Perform add operation on the shown filtered list ------------------------------ */
@@ -211,12 +216,12 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
 
         /* Case: missing name -> rejected */
         command = AddCommand.COMMAND_WORD + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
 
         /* Case: missing phone -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
 
         /* Case: invalid keyword -> rejected */
@@ -225,37 +230,37 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
 
         /* Case: invalid name -> rejected */
         command = AddCommand.COMMAND_WORD + INVALID_NAME_DESC + PHONE_DESC_APPLE + EMAIL_DESC_APPLE
-                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE;
+                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Name.MESSAGE_NAME_CONSTRAINTS);
 
         /* Case: invalid phone -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + INVALID_PHONE_DESC + EMAIL_DESC_APPLE
-                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE;
+                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Phone.MESSAGE_PHONE_CONSTRAINTS);
 
         /* Case: invalid email -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + INVALID_EMAIL_DESC
-                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE;
+                + ADDRESS_DESC_APPLE + PRICE_DESC_APPLE + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Email.MESSAGE_EMAIL_CONSTRAINTS);
 
         /* Case: invalid address -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE
-                + INVALID_ADDRESS_DESC + PRICE_DESC_APPLE + RATING_DESC_APPLE;
+                + INVALID_ADDRESS_DESC + PRICE_DESC_APPLE + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Address.MESSAGE_ADDRESS_CONSTRAINTS);
 
         /* Case: invalid price -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE
-                + ADDRESS_DESC_APPLE + INVALID_PRICE_DESC + RATING_DESC_APPLE;
+                + ADDRESS_DESC_APPLE + INVALID_PRICE_DESC + RATING_DESC_APPLE + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Price.MESSAGE_PRICE_CONSTRAINTS);
 
         /* Case: invalid rating -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE
-                + ADDRESS_DESC_APPLE + RATING_DESC_APPLE + INVALID_RATING_DESC;
+                + ADDRESS_DESC_APPLE + RATING_DESC_APPLE + INVALID_RATING_DESC + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Rating.MESSAGE_RATING_CONSTRAINTS);
 
         /* Case: invalid tag -> rejected */
         command = AddCommand.COMMAND_WORD + NAME_DESC_APPLE + PHONE_DESC_APPLE + EMAIL_DESC_APPLE + ADDRESS_DESC_APPLE
-                + PRICE_DESC_APPLE + RATING_DESC_APPLE + INVALID_TAG_DESC;
+                + PRICE_DESC_APPLE + RATING_DESC_APPLE + INVALID_TAG_DESC + ALLERGY_DESC_LACTOSE;
         assertCommandFailure(command, Tag.MESSAGE_TAG_CONSTRAINTS);
     }
 
