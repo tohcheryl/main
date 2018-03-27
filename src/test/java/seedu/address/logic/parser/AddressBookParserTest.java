d504536d8c243082d7cf929144400c17a328f734
Sam Xie <samxie@outlook.com.au>
2018-03-27 23:43:59 +0800

    Added order command to address book parser test

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -25,6 +25,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.OrderCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
@@ -107,6 +108,12 @@ public class AddressBookParserTest {
     public void parseCommand_list() throws Exception {
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+    }
+
+    @Test
+    public void parseCommand_order() throws Exception {
+        assertTrue(parser.parseCommand(OrderCommand.COMMAND_WORD) instanceof OrderCommand);
+        assertTrue(parser.parseCommand(OrderCommand.COMMAND_WORD + " 1") instanceof OrderCommand);
     }
 
     @Test
