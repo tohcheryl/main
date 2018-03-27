e2d2e86a1c9b5f6b59ac949870f709da06030d65
tohcheryl <e0035693@u.nus.edu>
2018-03-28 01:26:06 +0800

    Change UserConfigCommandTest

--- src/test/java/seedu/address/logic/commands/UserConfigCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UserConfigCommandTest.java
@@ -5,7 +5,12 @@ import static seedu.address.model.util.SampleDataUtil.getAllergySet;
 
 import org.junit.Test;
 
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.food.Address;
 import seedu.address.model.food.Name;
 import seedu.address.model.food.Phone;
@@ -13,15 +18,23 @@ import seedu.address.model.user.UserProfile;
 
 public class UserConfigCommandTest {
 
-
-    private AddressBook addressBook = new AddressBook();
+    private Model model = new ModelManager(new AddressBook(), new UserPrefs());
     private UserProfile validUserProfile = new UserProfile(new Name("Wei wei"), new Phone("92304333"),
                 new Address("Blk 343 Serangoon Ave 3 #23-23 Singapore 349343"), getAllergySet("banana"));
 
     @Test
-    public void execute_validUserProfile_setSuccessfully() {
-        addressBook.setUserProfile(validUserProfile);
-        assertEquals(validUserProfile, addressBook.getUserProfile());
+    public void execute_validUserProfile_setSuccessfully() throws Exception {
+        CommandResult commandResult = getUserConfigCommand(validUserProfile, model).execute();
+        assertEquals(UserConfigCommand.MESSAGE_SUCCESS, commandResult.feedbackToUser);
+        assertEquals(validUserProfile, model.getAddressBook().getUserProfile());
     }
 
+    /**
+     * Generates a new UserConfigCommand.
+     */
+    private UserConfigCommand getUserConfigCommand(UserProfile userProfile, Model model) {
+        UserConfigCommand command = new UserConfigCommand(userProfile);
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
 }
