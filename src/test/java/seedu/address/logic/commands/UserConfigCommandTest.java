74066602dcee00727bd892147defaa75aeac5465
tohcheryl <e0035693@u.nus.edu>
2018-03-28 00:37:19 +0800

    Add UserConfigCommandTest

---
+++ src/test/java/seedu/address/logic/commands/UserConfigCommandTest.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.model.util.SampleDataUtil.getAllergySet;
+import static seedu.address.testutil.TypicalFoods.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.food.Address;
+import seedu.address.model.food.Name;
+import seedu.address.model.food.Phone;
+import seedu.address.model.user.UserProfile;
+
+public class UserConfigCommandTest {
+
+
+    private AddressBook addressBook = getTypicalAddressBook();
+    private UserProfile validUserProfile = new UserProfile(new Name("Wei wei"), new Phone("92304333"),
+                new Address("Blk 343 Serangoon Ave 3 #23-23 Singapore 349343"), getAllergySet("banana"));
+
+    @Test
+    public void execute_validUserProfile_setSuccessfully() {
+        addressBook.setUserProfile(validUserProfile);
+        assertEquals(validUserProfile, addressBook.getUserProfile());
+    }
+
+}
