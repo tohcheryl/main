c1590140296a4c281c4ed8c6c2766f1d104682b7
Sam Xie <samxie@outlook.com.au>
2018-03-28 00:19:34 +0800

    Added basic test cases for Order command

---
+++ src/test/java/seedu/address/logic/commands/OrderCommandTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+
+public class OrderCommandTest {
+    private static final Index VALID_INDEX = Index.fromZeroBased(1);
+    private static final Index NULL_INDEX = null;
+
+    @Test
+    public void constructor_index_success() {
+        new OrderCommand(VALID_INDEX);
+        new OrderCommand(NULL_INDEX);
+    }
+
+    // TODO: Add stub class to test the order success
+    @Test
+    public void execute_order_success() throws CommandException {
+         OrderCommand orderCommand = new OrderCommand(VALID_INDEX);
+         // Below should be orderCommand.execute()
+         assertEquals(OrderCommand.MESSAGE_SUCCESS, OrderCommand.MESSAGE_SUCCESS);
+    }
+}
