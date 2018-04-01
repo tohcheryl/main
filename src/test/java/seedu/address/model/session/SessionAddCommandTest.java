6feefff87ceb288409c84a166c1454b2f8b6efc3
Jackson Huang <jackson_huang@live.com>
2018-04-01 23:06:21 +0800

    added success tests for all fields in SessionAddCommand

--- src/test/java/seedu/address/model/session/SessionAddCommandTest.java
+++ src/test/java/seedu/address/model/session/SessionAddCommandTest.java
@@ -6,7 +6,12 @@ import org.junit.rules.ExpectedException;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.model.food.Address;
+import seedu.address.model.food.Email;
 import seedu.address.model.food.Name;
+import seedu.address.model.food.Phone;
+import seedu.address.model.food.Price;
+import seedu.address.model.food.Rating;
 
 public class SessionAddCommandTest {
     @Rule
@@ -16,6 +21,36 @@ public class SessionAddCommandTest {
     public void parseInputForField_name_success() throws IllegalArgumentException, IllegalValueException {
         SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
         session.parseInputForField(Name.class, "Some Name");
+    }
+
+    @Test
+    public void parseInputForField_address_success() throws IllegalArgumentException, IllegalValueException {
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(Address.class, "Some Address");
+    }
+
+    @Test
+    public void parseInputForField_phone_success() throws IllegalArgumentException, IllegalValueException {
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(Phone.class, "123124913");
+    }
+
+    @Test
+    public void parseInputForField_email_success() throws IllegalArgumentException, IllegalValueException {
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(Email.class, "email@email.com");
+    }
+
+    @Test
+    public void parseInputForField_price_success() throws IllegalArgumentException, IllegalValueException {
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(Price.class, "12");
+    }
+
+    @Test
+    public void parseInputForField_rating_success() throws IllegalArgumentException, IllegalValueException {
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(Rating.class, "5");
     }
 
     @Test
