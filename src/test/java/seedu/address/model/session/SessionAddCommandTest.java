224b75ea1a9098b9a0e9320ec40c392f52473c3d
Jackson Huang <jackson_huang@live.com>
2018-04-01 21:29:23 +0800

    SessionAddCommandTest: Added tests for parseInputForField method

---
+++ src/test/java/seedu/address/model/session/SessionAddCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.model.session;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.model.food.Name;
+
+public class SessionAddCommandTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void parseInputForField_name_success() throws IllegalArgumentException, IllegalValueException {
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(Name.class, "Some Name");
+    }
+
+    @Test
+    public void parseInputForField_invalidClass_throwsIllegalArgumentException() throws IllegalArgumentException, IllegalValueException {
+        thrown.expect(IllegalArgumentException.class);
+        SessionAddCommand session = new SessionAddCommand(new AddCommand(null), null);
+        session.parseInputForField(String.class, "Some Input");
+    }
+}
