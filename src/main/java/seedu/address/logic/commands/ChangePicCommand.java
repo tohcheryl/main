5ed11fc6ca5c1b16f24b8c2b031f3f7e41e21896
tohcheryl <e0035693@u.nus.edu>
2018-04-05 01:54:20 +0800

    Update ChangePicCommand so that profile image is saved locally

--- src/main/java/seedu/address/logic/commands/ChangePicCommand.java
+++ src/main/java/seedu/address/logic/commands/ChangePicCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 //@@author {tohcheryl}
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javafx.stage.FileChooser;
 
@@ -10,9 +13,35 @@ import javafx.stage.FileChooser;
  * Changes the profile picture of the user
  */
 public class ChangePicCommand extends Command {
+
     public static final String COMMAND_WORD = "changepic";
 
     public static final String MESSAGE_PIC_CHANGED_ACKNOWLEDGEMENT = "Profile picture has been changed!";
+
+    /**
+     * Copies profile image file set by user and saves it into images folder
+     */
+    public void copyFile(File file) throws IOException {
+        int numBytes;
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        File outputFile = new File("src/main/resources/images/profilepic.png");
+        if (!outputFile.exists()) {
+            outputFile.createNewFile();
+        }
+        try {
+            fis = new FileInputStream(file);
+            fos = new FileOutputStream(outputFile);
+            while ((numBytes = fis.read()) != -1) {
+                fos.write(numBytes);
+            }
+            fis.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public CommandResult execute() {
@@ -20,8 +49,11 @@ public class ChangePicCommand extends Command {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files",
                 "*.png", "*.jpg", "*.gif"));
         File selectedFile = fileChooser.showOpenDialog(null);
-        //EventsCenter.getInstance().post(new ExitAppRequestEvent());
+        try {
+            copyFile(selectedFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new CommandResult(MESSAGE_PIC_CHANGED_ACKNOWLEDGEMENT);
     }
-
 }
