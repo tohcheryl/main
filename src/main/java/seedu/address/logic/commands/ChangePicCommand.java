ced45d5cf524fe02e35c8b6b3188d5f0d2c718db
tohcheryl <e0035693@u.nus.edu>
2018-04-06 00:36:20 +0800

    Remove copyFile method and moved it to FileUtil

--- src/main/java/seedu/address/logic/commands/ChangePicCommand.java
+++ src/main/java/seedu/address/logic/commands/ChangePicCommand.java
@@ -1,13 +1,11 @@
 package seedu.address.logic.commands;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import javafx.stage.FileChooser;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ProfilePictureChangedEvent;
+import seedu.address.commons.util.FileUtil;
 
 //@@author {tohcheryl}
 /**
@@ -19,34 +17,14 @@ public class ChangePicCommand extends Command {
 
     public static final String MESSAGE_PIC_CHANGED_ACKNOWLEDGEMENT = "Profile picture has been changed!";
 
-    /**
-     * Copies profile image file set by user and saves it
-     */
-    public void copyFile(File file) {
-        int numBytes;
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
-        File outputFile = new File("profilepic.png");
-        try {
-            fis = new FileInputStream(file);
-            fos = new FileOutputStream(outputFile);
-            while ((numBytes = fis.read()) != -1) {
-                fos.write(numBytes);
-            }
-            fis.close();
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public CommandResult execute() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files",
                 "*.png", "*.jpg", "*.gif"));
         File selectedFile = fileChooser.showOpenDialog(null);
-        copyFile(selectedFile);
+        File outputFile = new File("profilepic.png");
+        FileUtil.copyFile(selectedFile, outputFile);
         EventsCenter.getInstance().post(new ProfilePictureChangedEvent());
         return new CommandResult(MESSAGE_PIC_CHANGED_ACKNOWLEDGEMENT);
     }
