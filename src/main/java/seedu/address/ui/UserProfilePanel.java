f4d943824fd3eb832b86d887bbff4d329817ec32
tohcheryl <e0035693@u.nus.edu>
2018-04-08 15:41:39 +0800

    Wrap text and change background colour of profile panel

--- src/main/java/seedu/address/ui/UserProfilePanel.java
+++ src/main/java/seedu/address/ui/UserProfilePanel.java
@@ -11,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 
+import javafx.scene.shape.Circle;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.commons.events.ui.ProfilePictureChangedEvent;
@@ -28,6 +29,8 @@ public class UserProfilePanel extends UiPart<Region> {
     private static final Logger logger = LogsCenter.getLogger(UserProfilePanel.class);
 
     private static final String PROFILE_PICTURE_PATH = "profilepic.png";
+
+    final Circle clip = new Circle(75, 75, 75);
 
     private ReadOnlyAddressBook addressBook;
 
@@ -49,6 +52,9 @@ public class UserProfilePanel extends UiPart<Region> {
     public UserProfilePanel(ReadOnlyAddressBook addressBook) {
         super(FXML);
         this.addressBook = addressBook;
+        name.setWrapText(true);
+        phone.setWrapText(true);
+        address.setWrapText(true);
         setUserProfile(addressBook.getUserProfile());
         setProfilePicture();
         registerAsAnEventHandler(this);
@@ -64,6 +70,7 @@ public class UserProfilePanel extends UiPart<Region> {
 
     public void setProfilePicture() {
         profilepic.setImage(new Image("file:" + PROFILE_PICTURE_PATH));
+        profilepic.setClip(clip);
     }
 
     @Subscribe
