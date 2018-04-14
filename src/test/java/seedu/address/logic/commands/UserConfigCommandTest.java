5277cdba50db7da044b92b1ff34c77f69da1e43b
tohcheryl <e0035693@u.nus.edu>
2018-04-14 15:31:03 +0800

    Add test for UserConfigCommand

--- src/test/java/seedu/address/logic/commands/UserConfigCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UserConfigCommandTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
-import static seedu.address.model.util.SampleDataUtil.getAllergySet;
 
 import org.junit.Test;
 
@@ -11,23 +10,28 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.food.Address;
-import seedu.address.model.food.Name;
-import seedu.address.model.food.Phone;
 import seedu.address.model.user.UserProfile;
+import seedu.address.testutil.UserProfileBuilder;
 
 //@@author {tohcheryl}
 public class UserConfigCommandTest {
 
     private Model model = new ModelManager(new AddressBook(), new UserPrefs());
-    private UserProfile validUserProfile = new UserProfile(new Name("Wei wei"), new Phone("92304333"),
-                new Address("Blk 343 Serangoon Ave 3 #23-23 Singapore 349343"), getAllergySet("banana"));
+    private UserProfile validUserProfile = new UserProfileBuilder().build();
 
     @Test
     public void execute_validUserProfile_setSuccessfully() throws Exception {
         CommandResult commandResult = getUserConfigCommand(validUserProfile, model).execute();
         assertEquals(UserConfigCommand.MESSAGE_SUCCESS, commandResult.feedbackToUser);
-        assertEquals(validUserProfile, model.getAddressBook().getUserProfile());
+        assertEquals(validUserProfile, model.getUserProfile());
+    }
+
+    @Test
+    public void execute_duplicateUserProfile_setSuccessfully() throws Exception {
+        UserProfile currentUser = model.getUserProfile();
+        CommandResult commandResult = getUserConfigCommand(currentUser, model).execute();
+        assertEquals(UserConfigCommand.MESSAGE_SUCCESS, commandResult.feedbackToUser);
+        assertEquals(currentUser, model.getUserProfile());
     }
 
     /**
