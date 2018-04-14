36db6addc941891dfe0dea80381ca0f3a80c4474
Jackson Huang <jackson_huang@live.com>
2018-04-14 14:10:50 +0800

    added styles to label

--- src/main/java/seedu/address/ui/ChatPanel.java
+++ src/main/java/seedu/address/ui/ChatPanel.java
@@ -18,6 +18,9 @@ import seedu.address.commons.events.ui.NewUserMessageAvailableEvent;
  */
 public class ChatPanel extends UiPart<Region> {
     private static final int SPACING = 10;
+    private static final String RESULT_ERROR_STYLE = "result-error";
+    private static final String RESULT_SUCCESS_STYLE = "result-success";
+    private static final String USER_LABEL_STYLE = "user-label";
     private static final String FXML = "ChatPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ChatPanel.class);
 
@@ -36,15 +39,48 @@ public class ChatPanel extends UiPart<Region> {
         registerAsAnEventHandler(this);
     }
 
+    /**
+     * Creates a label for a result message.
+     *
+     * @param message      String message from system for user feedback.
+     * @param isSuccessful If the message represents a successful one.
+     * @return A new javafx Label object.
+     */
+    private Label createResultMessageLabel(String message, boolean isSuccessful) {
+        Label label = new Label(message);
+        label.setWrapText(true);
+        if (isSuccessful) {
+            label.getStyleClass().add(RESULT_SUCCESS_STYLE);
+        } else {
+            label.getStyleClass().add(RESULT_ERROR_STYLE);
+        }
+        return label;
+    }
+
+    /**
+     * Creates a label for a user message.
+     *
+     * @param message String message from user to the system.
+     * @return A new javafx Label object.
+     */
+    private Label createUserMessageLabel(String message) {
+        Label label = new Label(message);
+        label.setWrapText(true);
+        label.getStyleClass().add(USER_LABEL_STYLE);
+        return label;
+    }
+
 
     @Subscribe
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
-        chatPanel.getChildren().add(new Label(event.message));
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        chatPanel.getChildren().add(createResultMessageLabel(event.message, event.isSuccessful));
     }
 
     @Subscribe
     private void handleNewUserMessageEvent(NewUserMessageAvailableEvent event) {
-        chatPanel.getChildren().add(new Label(event.message));
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        chatPanel.getChildren().add(createUserMessageLabel(event.message));
     }
 
 }
